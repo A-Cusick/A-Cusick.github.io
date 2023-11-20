9e5d7d7159d0772f64202c807b3492373c16ac4c
A-Cusick <alex.cusick3@gmail.com>
2023-11-20 12:39:53 -0600

    adding more icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import kitty from './kitty512.png';
+import kitty from './assets/kitty512.png';
+import integral from './assets/integral128.png';
+import logarithm from './assets/logarithm128.png';
+import cone from './assets/cone128.png';
+import computer from './assets/computer128.png';
+import regression from './assets/regression128.png';
+import syntax from './assets/syntax128.png';
+import venn from './assets/venn128.png';
 import './App.css';
+
+const thoughtIcons = [integral, logarithm, cone, computer, regression, syntax, venn];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={kitty} className="Kitty" alt="kitty" />
+        <div id="thinking-kitty">
+          <img src={kitty} className="Kitty" alt="kitty" />
+          {thoughtIcons.map((thought, i) => {return <img src={thought} className="thought" id={`thought-${i}`} />})}
+        </div>
         <p>
           Hi! My portfolio is currently under construction :)
         </p>
