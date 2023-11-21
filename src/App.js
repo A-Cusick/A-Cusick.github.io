5f3193851a7e8d9a937826bd15d13f19eda23c40
A-Cusick <alex.cusick3@gmail.com>
2023-11-21 16:54:59 -0600

    move assets to module ooh fancy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import kitty from './assets/kitty512.png';
-import integral from './assets/integral128.png';
-import logarithm from './assets/logarithm128.png';
-import cone from './assets/cone128.png';
-import computer from './assets/computer128.png';
-import regression from './assets/regression128.png';
-import syntax from './assets/syntax128.png';
-import venn from './assets/venn128.png';
 import './App.css';
-
-const thoughtIcons = [integral, logarithm, cone, computer, regression, syntax, venn];
+import {thoughtIcons, kitty} from './assets/assets.js';
 
 function App() {
   return (
---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import kitty from './kitty512.png';
+import integral from './integral128.png';
+import logarithm from './logarithm128.png';
+import cone from './cone128.png';
+import computer from './computer128.png';
+import regression from './regression128.png';
+import syntax from './syntax128.png';
+import venn from './venn128.png';
+
+const thoughtIcons = [integral, logarithm, cone, computer, regression, syntax, venn]
+
+export {
+  thoughtIcons,
+  kitty
+}
